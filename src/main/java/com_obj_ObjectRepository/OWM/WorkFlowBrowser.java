3d8914940c4c8d67d3865e760df04d87a00c5169
X017406 <X017406@USE2DEVP0672.ead.thomsonreuters.com>
2020-12-10 17:49:40 +0530

    firstcommit

--- src/main/java/com_obj_ObjectRepository/OWM/WorkFlowBrowser.java
+++ src/main/java/com_obj_ObjectRepository/OWM/WorkFlowBrowser.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com_lib_FunctionLibrary.FrameWork;
-
+//comment
 public class WorkFlowBrowser {
 	WebDriver driver;
 	FrameWork fm = new FrameWork();
