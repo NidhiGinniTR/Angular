0494dd27234bd56cafc1f614df4028140dfed2fd
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2020-12-17 13:13:00 +0530

    Added comments in Tasks class.

--- src/test/java/com_pkg_OWM/Workflow_Browser/DynamicSerial/FWF_Tasks_Test.java
+++ src/test/java/com_pkg_OWM/Workflow_Browser/DynamicSerial/FWF_Tasks_Test.java
@@ -1,5 +1,38 @@
 package com_pkg_OWM.Workflow_Browser.DynamicSerial;
 
-public class FWF_Tasks_Test {
+import java.io.IOException;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import com_helper_Reporting.ExtentManager;
+import com_data_Resources.Environment.BrowserInvoke;
+
+
+public class FWF_Tasks_Test extends BrowserInvoke {
+	ExtentTest test;
+
+	@BeforeSuite
+	public void beforeStart() {
+		ExtentManager.createInstance();
+	}
+
+	public void Initialize() throws IOException {
+		driver = InvokeDriver();
+		driver.get(propEnv.getProperty("URL"));
+	}
+
+	@Test(dependsOnMethods = "Initialize")
+	public void Tasks() throws InterruptedException {
+		//--------Login--------------------------------
+		//--------Launch WorkFlow Manager--------------
+		//-------Navigate to Respective Tab------------
+		//-------Search for the Required Workflow------
+		//-------DoubleClick on the Workflow ~ FolderWorkFows---
+		//-------Click on Tasks Tab--------------------
+		//------Actions Menu---------------------------
+	}
 
 }
