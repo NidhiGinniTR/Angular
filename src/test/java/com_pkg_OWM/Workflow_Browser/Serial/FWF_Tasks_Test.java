3bb440d220cdad36fe058f7dcc6f7d5e4cf7200d
X017406 <X017406@USE2DEVP0672.ead.thomsonreuters.com>
2020-12-21 19:18:25 +0530

    some script changed

--- src/test/java/com_pkg_OWM/Workflow_Browser/Serial/FWF_Tasks_Test.java
+++ src/test/java/com_pkg_OWM/Workflow_Browser/Serial/FWF_Tasks_Test.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -22,6 +24,7 @@ public class FWF_Tasks_Test extends BrowserInvoke {
 		ExtentManager.createInstance();
 	}
 	
+	@Test
 	public void Initialize() throws IOException {
 		driver = InvokeDriver();
 		driver.get(propEnv.getProperty("URL"));
@@ -34,16 +37,16 @@ public class FWF_Tasks_Test extends BrowserInvoke {
 			FunctionLibrary.fnLogin(driver, propEnv);
 			
 		//Step-2:-----Launch WorkFlow Manager---------------------------//
-			FunctionLibrary.fnlaunchApplication(driver, "WorkFlow Manager");
+			FunctionLibrary.fnLaunchApplication(driver, "WorkFlow Manager");
 			Thread.sleep(3000);
 			
 		//Step-3:-----Navigate to Respective Tab------------------------//
 			new WebDriverWait(driver,50).until(ExpectedConditions.numberOfWindowsToBe(2));
-			FunctionLibrary.fnNavigateToTab(driver, "WorkFlow Browser");
+			FunctionLibrary.fnNavigateTab(driver, "WorkFlow Browser");
 			Thread.sleep(900);
 			
 		//Step-4:-----Search for the Required Workflow------------------//
-			FunctionLibrary.fnWorkFlowBrowserSearch(driver, propParallelData);
+			FunctionLibrary.fnWorkflowBrowserSearch(driver, propSerialData);
 			Thread.sleep(3000);
 			
 		//Step-5:-----Double click on the workflow to Folder Workflows---//
@@ -66,5 +69,15 @@ public class FWF_Tasks_Test extends BrowserInvoke {
 		//Step-17:----Save Preferences for All--------------------------//
 		//Step-18:----LogOff-------------------------------------------//
 		
+	}
+	
+	@AfterClass
+	void closeBrowser() throws InterruptedException {
+		//FunctionLibrary.fnLogOff(driver);
+	}
+
+	@AfterSuite
+	public void aftersuite() {
+		ExtentManager.endReport();
 	}
 }
--- src/test/java/com_pkg_OWM/Workflow_Browser/Serial/OWM_Actions_Test.java
+++ src/test/java/com_pkg_OWM/Workflow_Browser/Serial/OWM_Actions_Test.java
@@ -1,12 +1,18 @@
 package com_pkg_OWM.Workflow_Browser.Serial;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com_data_Resources.Environment.BrowserInvoke;
 import com_helper_Reporting.ExtentManager;
+import com_lib_FunctionLibrary.FunctionLibrary;
 
 public class OWM_Actions_Test extends BrowserInvoke {
 	
@@ -15,13 +21,37 @@ public class OWM_Actions_Test extends BrowserInvoke {
 		ExtentManager.createInstance();
 	}
 	
+	@Test
 	public void Initialize() throws IOException {
 		driver = InvokeDriver();
 		driver.get(propEnv.getProperty("URL"));
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 	
 	@Test(dependsOnMethods = "Initialize")
 	public void Tasks() throws InterruptedException {
+	//Step-1:-----Login---------------------------------------------//
+		FunctionLibrary.fnLogin(driver, propEnv);
+		
+	//Step-2:-----Launch WorkFlow Manager---------------------------//
+		FunctionLibrary.fnLaunchApplication(driver, "WorkFlow Manager");
+		Thread.sleep(3000);
+		
+	//Step-3:-----Navigate to Respective Tab------------------------//
+		new WebDriverWait(driver,50).until(ExpectedConditions.numberOfWindowsToBe(2));
+		FunctionLibrary.fnNavigateTab(driver, "WorkFlow Browser");
+		Thread.sleep(900);
+		
+	//Step-4:-----Search for the Required Workflow------------------//
+		FunctionLibrary.fnWorkflowBrowserSearch(driver, propSerialData);
+		Thread.sleep(3000);
+		
+	//Step-5:-----Double click on the workflow to Folder Workflows---//
+		driver.switchTo().frame("viewIFrame");
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//*[@id='grdWFfolders_dom']/table/tbody/tr[3]/td"))).doubleClick().build().perform();
+		Thread.sleep(10000);
+		
 		//--------------------------------------------Login----------------------------------------------------------------------
 		//-------------------------------------Launch WorkFlow Manager-----------------------------------------------------------
 		//-----------------------------------Navigate to Respective Tab----------------------------------------------------------
