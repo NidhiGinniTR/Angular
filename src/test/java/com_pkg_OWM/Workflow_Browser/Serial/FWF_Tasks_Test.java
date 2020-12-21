74cfa13019e6ae6c48ae74d56e5e44698062fa3d
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2020-12-21 20:23:36 +0530

    Updated Tasks class

--- src/test/java/com_pkg_OWM/Workflow_Browser/Serial/FWF_Tasks_Test.java
+++ src/test/java/com_pkg_OWM/Workflow_Browser/Serial/FWF_Tasks_Test.java
@@ -1,6 +1,7 @@
 package com_pkg_OWM.Workflow_Browser.Serial;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -14,7 +15,6 @@ import org.testng.annotations.Test;
 
 import com_data_Resources.Environment.BrowserInvoke;
 import com_helper_Reporting.ExtentManager;
-import com_lib_FunctionLibrary.FunctionLibrary;
 import com_lib_FunctionLibrary.FunctionLibrary;
 
 public class FWF_Tasks_Test extends BrowserInvoke {
@@ -55,17 +55,42 @@ public class FWF_Tasks_Test extends BrowserInvoke {
 			Thread.sleep(10000);
 			
 		//Step-6:-----Click on Tasks Tab and Actions--------------------//
+			new WebDriverWait(driver,50).until(ExpectedConditions.numberOfWindowsToBe(3));
+			driver.switchTo().parentFrame();
+			Set<String> ids = driver.getWindowHandles();
+	        java.util.Iterator<String> it = ids.iterator();
+	        String parentid = it.next();
+	        String childid = it.next();
+	        String childid1 = it.next();
+	        driver.switchTo().window(childid1);
+	        Thread.sleep(8000);
+	        driver.switchTo().frame("tabIFrame");
+	        action.moveToElement(driver.findElement(By.xpath("//DIV[@id='grdTasks_dom']/table/tbody/tr[2]"))).click().build().perform();
+	        Thread.sleep(300);
+	        driver.findElement(By.xpath("//td[@id='btnActionsMenu']")).click();
+			Thread.sleep(300);
 		//Step-7:-----Change Status-------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Change Status");
 		//Step-8:-----Route Task----------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Route Task");
 		//Step-9:-----Task Properties-----------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Task Properties");
 		//Step-10:----Task History--------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Task History");
 		//Step-11:----Reset Checklist-----------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Reset Checklist");
 		//Step-12:----Add Documents-------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Add Document");
 		//Step-13:----View Documents------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"View Document(s)");
 		//Step-14:----Export--------------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Export");
 		//Step-15:----Customize View-------------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Customize View");
 		//Step-16:----Save Preferences----------------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Save Preferences");
 		//Step-17:----Save Preferences for All--------------------------//
+	        FunctionLibrary.fnOWMActionsMenu(driver,"Save Preferences for All");
 		//Step-18:----LogOff-------------------------------------------//
 		
 	}
