e1bd43cb2c84b79e04bb9814e1e76fdc89f649bb
X017406 <X017406@USE2DEVP0672.ead.thomsonreuters.com>
2020-12-21 15:30:22 +0530

    Added Commits in OWM Serial Actions

--- src/test/java/com_pkg_OWM/Workflow_Browser/Serial/OWM_Actions_Test.java
+++ src/test/java/com_pkg_OWM/Workflow_Browser/Serial/OWM_Actions_Test.java
@@ -1,5 +1,42 @@
 package com_pkg_OWM.Workflow_Browser.Serial;
 
-public class OWM_Actions_Test {
+import java.io.IOException;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com_data_Resources.Environment.BrowserInvoke;
+import com_helper_Reporting.ExtentManager;
+
+public class OWM_Actions_Test extends BrowserInvoke {
+	
+	@BeforeSuite
+	public void beforeStart() {
+		ExtentManager.createInstance();
+	}
+	
+	public void Initialize() throws IOException {
+		driver = InvokeDriver();
+		driver.get(propEnv.getProperty("URL"));
+	}
+	
+	@Test(dependsOnMethods = "Initialize")
+	public void Tasks() throws InterruptedException {
+		//--------------------------------------------Login----------------------------------------------------------------------
+		//-------------------------------------Launch WorkFlow Manager-----------------------------------------------------------
+		//-----------------------------------Navigate to Respective Tab----------------------------------------------------------
+		//----------------------------------Create a new Serial Workflow---------------------------------------------------------
+		//--------------------------------Search for the Required Workflow-------------------------------------------------------
+		//----------------------Verifying the WorkFlow Properties window fields with our data------------------------------------
+		//----------------------Verifying the WorkFlow History window and exporting the data-------------------------------------
+		//--------------Locking the particular WorkFlow and verifying the same in WorkFlow Properties window---------------------
+		//-------------------------------------Unlocking the WorkFlow------------------------------------------------------------
+		//-------------------------------Exporting the WorkFlow Browser records--------------------------------------------------
+		//------------------------------Setting the Customize View window as required--------------------------------------------
+		//------------------------------Saving the preferences in the WorkFlow Browser-------------------------------------------
+		//-----------------------Saving the preferences in the WorkFlow Browser for all users------------------------------------
+		//----------------------------Clicking on New WorkFlow and creating a new workflow---------------------------------------
+		
+	}
 
 }
