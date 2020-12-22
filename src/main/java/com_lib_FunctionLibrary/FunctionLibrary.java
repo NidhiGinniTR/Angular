2b3680ffa2484c7520fa18e4c9fcc26715232471
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2020-12-22 15:49:34 +0530

    Added method for customize view, save preferences and modified other methods.

--- src/main/java/com_lib_FunctionLibrary/FunctionLibrary.java
+++ src/main/java/com_lib_FunctionLibrary/FunctionLibrary.java
@@ -5,8 +5,14 @@ import java.util.Properties;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.aventstack.extentreports.Status;
 
 import com_helper_Reporting.ExtentManager;
 import com_obj_ObjectRepository.FolderWorkFlows.NavigationTabs;
@@ -15,23 +21,26 @@ import com_obj_ObjectRepository.LS1.OneSourceLogin;
 import com_obj_ObjectRepository.OWM.Navigations;
 import com_obj_ObjectRepository.OWM.WorkFlowBrowser;
 
-public class FunctionLibrary extends ExtentManager {
-	
-	public static void fnLogin(WebDriver driver, Properties Env) throws InterruptedException {
-		childTest = test.createNode("Login Universal ID and Password. <br> << Screen Name : Login Page>>");
-		OneSourceLogin Lp = new OneSourceLogin(driver);
-		Lp.reloadPage();
-		Lp.enterUsername(Env.getProperty("UniversalId"));
-		Lp.enterPassword(Env.getProperty("Password"));
-		Lp.clickonSignIn();
 
+public class FunctionLibrary extends ExtentManager {
+
+	public static void fnLogin(WebDriver driver, Properties Env) throws InterruptedException {
+		childTest = test.createNode("Description: Entering UniversalID and Password." + "<br>"
+				+ "<< Screen Name : OneSource Login Page >></br>");
+		if (driver.getTitle().equalsIgnoreCase("ONESOURCE") || driver.getTitle().equalsIgnoreCase("THOMSON REUTERS")) {
+			OneSourceLogin Lp = new OneSourceLogin(driver);
+			Lp.reloadPage();
+			Lp.enterUsername(Env.getProperty("UniversalId"));
+			Lp.enterPassword(Env.getProperty("Password"));
+			Lp.clickonSignIn();
+		}
 	}
 
-	public static void fnLaunchApplication(WebDriver driver, String strAppName) throws InterruptedException
-	{
-		childTest = test.createNode("Launching Application. <br> << Screen Name : LS1 Dashboard>>");
+	public static void fnLaunchApplication(WebDriver driver, String strAppName) throws InterruptedException {
+		childTest = test.createNode("Description: Launching " + strAppName + " from Applications." + "<br>"
+				+ "<< Screen Name: OneSource Dashboard >></br>");
 		driver.switchTo().frame("header");
-		if(driver.getTitle().equalsIgnoreCase("ONESOURCE")) {
+		if (driver.getTitle().equalsIgnoreCase("ONESOURCE")) {
 			driver.switchTo().frame("header");
 			OneSourceDashboard Lp = new OneSourceDashboard(driver);
 			Lp.launchApplications();
@@ -42,10 +51,10 @@ public class FunctionLibrary extends ExtentManager {
 			System.out.println(strAppName + "is selected from Applications dropdown");
 		}
 	}
-	
-	public static void fnNavigateTab(WebDriver driver, String tab) throws InterruptedException 
-	{
-		childTest = test.createNode("Navigation Tab. <br> << Screen Name : OWM Page>>");
+
+	public static void fnNavigateTab(WebDriver driver, String tab) throws InterruptedException {
+		childTest = test
+				.createNode("Description: Navigating to: " + tab + "." + "<br>" + "<< Screen Name: OWM >></br>");
 		Set<String> ids = driver.getWindowHandles();
 		java.util.Iterator<String> it = ids.iterator();
 		String parentid = it.next();
@@ -95,13 +104,12 @@ public class FunctionLibrary extends ExtentManager {
 					Nt.tabDataFlow();
 					break;
 				}
-				System.out.println("Navigated to: " + tab);
+				childTest.log(Status.PASS, "Navigated to " + tab);
 			}
 		}
 	}
-	
-	public static void fnOWMActionsMenu(WebDriver driver, String item,String subItem) throws InterruptedException 
-	{
+
+	public static void fnOWMActionsMenu(WebDriver driver, String item, String subItem) throws InterruptedException {
 		driver.switchTo().defaultContent();
 		driver.switchTo().frame("bottom");
 		driver.switchTo().frame("content");
@@ -117,18 +125,18 @@ public class FunctionLibrary extends ExtentManager {
 				if (item.equals(menuitems.get(i).getText())) {
 					String menuitem = menuitems.get(i).getAttribute("id");
 					driver.findElement(By.id(menuitem)).click();
-					if(!subItem.isEmpty()) {
-						List<WebElement> submenuItems = driver.findElements(By.className("MenuItemHover"));
-						for(int j=0;j<submenuItems.size();j++) {
-							if(subItem.equals(submenuItems.get(j).getText())) {
-							String submenuItem = submenuItems.get(j).getAttribute("id");
-							driver.findElement(By.id(submenuItem)).click();
-							System.out.println(subItem+ " is selected from"+item);
-							break;
+					if (!subItem.isEmpty()) {
+						List<WebElement> submenuItems = driver.findElements(By.className("MenuItem"));
+						for (int j = 0; j < submenuItems.size(); j++) {
+							if (subItem.equals(submenuItems.get(j).getText())) {
+								String submenuItem = submenuItems.get(j).getAttribute("id");
+								driver.findElement(By.id(submenuItem)).click();
+								childTest.log(Status.PASS, subItem+" is selected from  " +item);
+								break;
 							}
 						}
 					}
-					System.out.println(item + " is enabled and is selected from actions menu");
+					childTest.log(Status.PASS, item+" is enabled and is selected from actions menu.");
 					break;
 				}
 			}
@@ -136,8 +144,8 @@ public class FunctionLibrary extends ExtentManager {
 	}
 
 	public static void fnNewFolderCreation(WebDriver driver, Properties prop) throws InterruptedException {
-		childTest = test.createNode("New Folder Creation. <br> << Screen Name : NewFolder Page>>");
-
+		childTest = test.createNode(
+				"Description: Creating a New Folder." + "<br>" + "<< Screen Name: New Folder Webpage >></br>");
 		Set<String> ids1 = driver.getWindowHandles();
 		java.util.Iterator<String> it1 = ids1.iterator();
 		String parentid = it1.next();
@@ -159,10 +167,55 @@ public class FunctionLibrary extends ExtentManager {
 			driver.quit();
 		}
 		Nf.newfolderSave();
+		if (isAlertPresent(driver)) {
+			if (driver.switchTo().alert().getText().equalsIgnoreCase("A Folder already exists")) {
+				driver.switchTo().alert().accept();
+				driver.switchTo().window(childid1).close();
+				childTest.log(Status.INFO,
+						"A folder already Exists with the given data<br>Creating a new folder by deleting the existing.</br>");
+				fnWorkflowBrowserSearch(driver, prop);
+				Thread.sleep(4000);
+				driver.switchTo().frame("viewIFrame");
+				Actions action = new Actions(driver);
+				action.moveToElement(
+						driver.findElement(By.xpath("//*[@id='grdWFfolders_dom']/table/tbody/tr[3]/td"))).click()
+						.build().perform();
+				Thread.sleep(500);
+				driver.switchTo().defaultContent();
+				driver.switchTo().frame("bottom");// Switch to respective window
+				driver.switchTo().frame("content");
+				driver.switchTo().frame("bottomFrame");
+				fnOWMActionsMenu(driver, "Delete WorkFlow(s)", "");
+				driver.switchTo().alert().accept();
+				Thread.sleep(500);
+				driver.switchTo().defaultContent();
+				driver.switchTo().frame("bottom");// Switch to respective window
+				driver.switchTo().frame("content");
+				driver.switchTo().frame("bottomFrame");
+				fnOWMActionsMenu(driver, "New Folder", "");
+				Thread.sleep(5000);
+				new WebDriverWait(driver, 50).until(ExpectedConditions.numberOfWindowsToBe(3));
+				fnNewFolderCreation(driver, prop);
+			}
+		} else {
+			Thread.sleep(4000);
+			System.out.println("New Folder has been successfully created");
+			driver.switchTo().defaultContent();
+		}
 	}
 
+	public static boolean isAlertPresent(WebDriver driver) {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+	
 	public static void fnWorkflowBrowserSearch(WebDriver driver, Properties prop) throws InterruptedException {
-		childTest = test.createNode("Search Fields. <br> << Screen Name : Workflow Browser Page>>");
+		childTest = test.createNode(
+				"Description: Searching workflow with required fields" + "<br>" + "<<Screen Name: OWM >></br>");
 		Set<String> ids = driver.getWindowHandles();
 		java.util.Iterator<String> it = ids.iterator();
 		String parentid = it.next();
@@ -171,7 +224,6 @@ public class FunctionLibrary extends ExtentManager {
 		driver.switchTo().frame("bottom");
 		driver.switchTo().frame("content");
 		driver.switchTo().frame("bottomFrame");
-
 		WorkFlowBrowser Sf = new WorkFlowBrowser(driver);
 		Sf.SearchClear();
 		Sf.SearchYear(prop.getProperty("Year"));
@@ -181,24 +233,81 @@ public class FunctionLibrary extends ExtentManager {
 		Sf.SearchEntityName(prop.getProperty("Entity_Name"));
 		Sf.SearchEntityID(prop.getProperty("Entity_Id"));
 		Sf.SearchJurisdiction(prop.getProperty("Jurisdiction"));
-		//Sf.SearchWorkflowAssociation(prop.getProperty("Workflow_Association"));
+		// Sf.SearchWorkflowAssociation(prop.getProperty("Workflow_Association"));
 		Sf.SearchWorkflowType(prop.getProperty("Workflow_Type"));
-		//Sf.SearchGroupCodes(prop.getProperty("Group_Codes"));
+		// Sf.SearchGroupCodes(prop.getProperty("Group_Codes"));
 		Sf.Search();
 	}
 
-	public static void fnFWFSwitchingTab(WebDriver driver,String tab) throws InterruptedException 
-	{
+	public static void fnFWFSwitchingTab(WebDriver driver, String tab) throws InterruptedException {
 		Set<String> ids = driver.getWindowHandles();
-        java.util.Iterator<String> it = ids.iterator();
-        String parentid = it.next();
-        String childid = it.next();
-        String childid1 = it.next();
-        Thread.sleep(500);
-        driver.switchTo().window(childid1);
-        Thread.sleep(500);
-        NavigationTabs St = new NavigationTabs(driver);
+		java.util.Iterator<String> it = ids.iterator();
+		String parentid = it.next();
+		String childid = it.next();
+		String childid1 = it.next();
+		Thread.sleep(500);
+		driver.switchTo().window(childid1);
+		Thread.sleep(500);
+		NavigationTabs St = new NavigationTabs(driver);
 		St.fwf_tabSelection(tab);
 	}
 
+	public static void fnOWMSavePreferences(WebDriver driver, String menuitem) throws InterruptedException {
+		childTest = test.createNode("Description: Save Preferences For All" + "<br>" + "<< Screen Name: OWM >></br>");
+		WorkFlowBrowser owm = new WorkFlowBrowser(driver);
+		Set<String> ids1 = driver.getWindowHandles();
+		java.util.Iterator<String> it1 = ids1.iterator();
+		String parentid = it1.next();
+		String childid = it1.next();
+		String childid1 = it1.next();
+		driver.switchTo().window(childid1);
+		if (menuitem.equals("Save Preferences for All")) {
+			if (driver.getTitle().equals("Save Preferences")) {
+				System.out.println(driver.getTitle());
+				owm.SavePreforAll_Notify();
+				Thread.sleep(1000);
+				owm.SavePreforAll_Save();
+				Thread.sleep(3000);
+				driver.switchTo().window(childid);
+				if (driver.switchTo().alert().getText().contains("Screen preferences changed for all users")) {
+					driver.switchTo().alert().accept();
+					childTest.log(Status.PASS, "Clicked on OK in the Alert Popup");
+				} else {
+					childTest.log(Status.ERROR, "Missing Confirmation Message / Alert Popup.");
+				}
+			}
+		} else if (menuitem.equals("Save Preferences")) {
+			driver.switchTo().window(childid);
+			if (driver.switchTo().alert().getText().contains("Your changes have been saved")) {
+				driver.switchTo().alert().accept();
+				childTest.log(Status.PASS, "Clicked on OK in the Alert Popup");
+			} else {
+				childTest.log(Status.ERROR, "Missing Confirmation Message / Alert Popup.");
+			}
+		}
+	}
+	
+	public static void fnOWMCustomizeView(WebDriver driver, String[] array) throws InterruptedException {
+		childTest = test.createNode("Description: Customize View" + "<br>" + "<< Screen Name: OWM >></br>");
+		Set<String> ids1 = driver.getWindowHandles();
+		java.util.Iterator<String> it1 = ids1.iterator();
+		String parentid = it1.next();
+		String childid = it1.next();
+		String childid1 = it1.next();
+		driver.switchTo().window(childid1);
+		WorkFlowBrowser owm = new WorkFlowBrowser(driver);
+		if (driver.getTitle().equalsIgnoreCase("Grid Columns")) {
+			for (int i = 0; i < array.length; i++) {
+				if (driver.findElement(By.xpath("//span/input[contains(..,'" + array[i] + "')]")).isSelected()) {
+					childTest.log(Status.PASS, array[i] + " is checked.");
+				} else {
+					driver.findElement(By.xpath("//span/input[contains(..,'" + array[i] + "')]")).click();
+					childTest.log(Status.PASS, array[i] + " is checked.");
+				}
+			}
+			owm.Custview_Save();
+		} else {
+			childTest.log(Status.FAIL, "Customize view window failed to Open/Not in focus mode");
+		}
+	}
 }
