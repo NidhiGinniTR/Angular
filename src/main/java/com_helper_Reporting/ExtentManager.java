011320489e4ae9b6c7ebf7c94e17618bfb17a569
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2021-03-23 18:27:21 +0530

    Test run reports

--- src/main/java/com_helper_Reporting/ExtentManager.java
+++ src/main/java/com_helper_Reporting/ExtentManager.java
@@ -2,6 +2,7 @@ package com_helper_Reporting;
 
 import java.io.File;
 import java.util.Date;
+import java.util.Properties;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -11,12 +12,15 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ExtentManager {
+import com_data_Resources.Environment.BrowserInvoke;
+
+public class ExtentManager extends BrowserInvoke{
 
 	public static ExtentHtmlReporter htmlReporter;
 	public static ExtentReports extent;
 	public static ExtentTest test;
 	public static ExtentTest childTest;
+	
 
 	@BeforeClass
 	public static ExtentReports createInstance() {
@@ -25,11 +29,9 @@ public class ExtentManager {
 		new File(directory).mkdirs();
 		String path = directory + fileName;
 		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(path);
-		
 		htmlReporter.config().setDocumentTitle("Automation Test Report");
-		htmlReporter.config().setReportName("New Folder Creation");
+		htmlReporter.config().setReportName("Automation-QA");
 		htmlReporter.config().setTheme(Theme.STANDARD);
-
 		extent = new ExtentReports();
 		extent.setSystemInfo("Organization", "TechMahindra-TR");
 		extent.setSystemInfo("HostName", System.getenv("COMPUTERNAME"));
