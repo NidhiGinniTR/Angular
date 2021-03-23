abbe128d64361c269bb9a92ac076ecd1c172e514
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2021-03-23 13:24:43 +0530

    Test Run Reports

--- src/test/java/com_pkg_OSP/EntityUnitBrowser_Ownership_StockOfTheCompany_Test.java
+++ src/test/java/com_pkg_OSP/EntityUnitBrowser_Ownership_StockOfTheCompany_Test.java
@@ -4,12 +4,19 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchFrameException;
+import org.openqa.selenium.NoSuchWindowException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -43,6 +50,11 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 		EntityUnitBrowser em = new EntityUnitBrowser(driver, propSerialData,propSerialData);
 		FrameWork fm = new FrameWork();
 		// OWM owm = new OWM(driver,propSerialData);
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(50))
+				.pollingEvery(Duration.ofSeconds(5))
+				.ignoring(NoSuchElementException.class,NoSuchWindowException.class)
+				.ignoring(NoSuchFrameException.class);
 		// Step-1:-----Login---------------------------------------------//
 		lp.fnLogin();
 
@@ -50,22 +62,23 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 		lp.LaunchApplication("Entity Manager");
 		Thread.sleep(5000);
 		driver.switchTo().defaultContent();
-		driver.switchTo().frame("maincontent");
-		driver.switchTo().frame("app_frame_a01b96d5-d9c7-455c-98a9-b084156123ad");
-		driver.switchTo().frame("gridFrame");
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("maincontent"));
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("app_frame_a01b96d5-d9c7-455c-98a9-b084156123ad"));
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("gridFrame"));
 		em.fnSearchEntity();
 		// Double click on required workflow
 		fm.fnWebTable(driver, driver.findElement(By.xpath("//tr[@id='gridEntityBrowser_grdEntityManager_row_0']")),
 				"DoubleClick");
 		driver.switchTo().defaultContent();
 		Thread.sleep(2000);
+		wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 		lp.fnSwitchtoWindow(2, "Entity Information");
 
 		// Step----------Navigateto Ownership tab to Stock Of The Company--------------------------//
 		fm.fnWebButton(driver, By.xpath("//table[@id='TabStrip1_1']//nobr"), "Ownership");
 		Thread.sleep(1000);
 		fm.fnWebButton(driver, By.xpath("//label[@id='lblStock']"), "Stock Of The Company");
-		driver.switchTo().frame("fraContent2");
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 		List<WebElement> rows = driver
 				.findElements(By.xpath("//DIV[@id='gridSc_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR"));
 		if (rows.size() >= 2) {
@@ -88,27 +101,31 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 		// Step----------Add New-------------------------------//
 		fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"),"Actions");
 		lp.fnOWMActionsMenu("Add New", "");
-		Thread.sleep(3000);
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.numberOfWindowsToBe(3));
 		lp.fnSwitchtoWindow(3, "Entity Manager");
 			//Creating a Related Share Holder
 				em.fnEMStockOfCompanyAddNew("Related");
 				Thread.sleep(2000);
 			//Creating a Unrelated Share Holder
+				wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 				lp.fnSwitchtoWindow(2, "Entity Information");
-				driver.switchTo().frame("fraContent2");
+				wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 				fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"),"Actions");
 				lp.fnOWMActionsMenu("Add New", "");
-				Thread.sleep(3000);
+				Thread.sleep(1000);
+				wait.until(ExpectedConditions.numberOfWindowsToBe(3));
 				lp.fnSwitchtoWindow(3, "Entity Manager");
 				em.fnEMStockOfCompanyAddNew("UnRelated");
 				
 		// Step-----------Edit/View Details-------------------//
+				wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 		lp.fnSwitchtoWindow(2, "Entity Information");
 		fm.fnWebButton(driver, By.xpath("//label[@id='lblOrg']"), "Org-Reorg");
 		Thread.sleep(1500);
 		fm.fnWebButton(driver, By.xpath("//label[@id='lblStock']"), "Stock Of The Company");
 		Thread.sleep(2500);
-		driver.switchTo().frame("fraContent2");
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 		List<WebElement> rowsafter = driver
 				.findElements(By.xpath("//DIV[@id='gridSc_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR"));
 		System.out.println(rowsafter.size());
@@ -120,11 +137,14 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 						"Click");
 				fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"),"Actions");
 				lp.fnOWMActionsMenu("Edit/View Details", "");
-				Thread.sleep(3000);
+				Thread.sleep(1000);
+				wait.until(ExpectedConditions.numberOfWindowsToBe(3));
 				lp.fnSwitchtoWindow(3, "Entity Manager");
 				em.fnEMStockOfCompanyEdit(i);
+				Thread.sleep(1000);
+				wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 				lp.fnSwitchtoWindow(2, "Entity Information");
-				driver.switchTo().frame("fraContent2");
+				wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 				
 			}
 		}
@@ -135,17 +155,19 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 		Thread.sleep(1000);
 		fm.fnWebButton(driver, By.xpath("//label[@id='lblStock']"), "Stock Of The Company");
 		Thread.sleep(1500);
-		driver.switchTo().frame("fraContent2");
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 		fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"),"Actions");
 		lp.fnOWMActionsMenu("Customize View", "");
-		Thread.sleep(3000);
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.numberOfWindowsToBe(3));
 		lp.fnSwitchtoWindow(3, "Customize View");
 		String[] array = new String[] { "Class of Stock", "Beginning of Tax Year", "End of Tax Year", "Name of Stock Holder","ID of Stock Holder"};
 		em.fnCustomizeView(array);
 
 		// Step----------Save Preferences------------------------//
+		wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 		lp.fnSwitchtoWindow(2, "Entity Information");
-		driver.switchTo().frame("fraContent2");
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
 		fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"),"Actions");
 		lp.fnOWMActionsMenu("Save Preferences", "");
 		em.fnSavePreferences("Save Preferences");
@@ -175,6 +197,7 @@ public class EntityUnitBrowser_Ownership_StockOfTheCompany_Test extends BrowserI
 
 		}
 		driver.close();
+		wait.until(ExpectedConditions.numberOfWindowsToBe(1));
 		lp.fnSwitchtoWindow(1, "Onesource");
 
 		// Step--------LogOFF-------------------------------//
