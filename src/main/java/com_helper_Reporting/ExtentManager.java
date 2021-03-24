dab6e245b915545cd7ca12781c5832ef331f8228
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2021-03-24 13:11:52 +0530

    Edited ExtentManager class

--- src/main/java/com_helper_Reporting/ExtentManager.java
+++ src/main/java/com_helper_Reporting/ExtentManager.java
@@ -2,8 +2,6 @@ package com_helper_Reporting;
 
 import java.io.File;
 import java.util.Date;
-import java.util.Properties;
-
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -12,9 +10,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-import com_data_Resources.Environment.BrowserInvoke;
-
-public class ExtentManager extends BrowserInvoke{
+public class ExtentManager{
 
 	public static ExtentHtmlReporter htmlReporter;
 	public static ExtentReports extent;
@@ -24,13 +20,14 @@ public class ExtentManager extends BrowserInvoke{
 
 	@BeforeClass
 	public static ExtentReports createInstance() {
+		Date d = new Date();
 		String fileName = getReportName();
 		String directory = System.getProperty("user.dir") + "/reports.html/";
 		new File(directory).mkdirs();
 		String path = directory + fileName;
 		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(path);
 		htmlReporter.config().setDocumentTitle("Automation Test Report");
-		htmlReporter.config().setReportName("Automation-QA");
+		htmlReporter.config().setReportName("Automation-QA/"+d.toString().replace(":", "_").replace(" ", "_"));
 		htmlReporter.config().setTheme(Theme.STANDARD);
 		extent = new ExtentReports();
 		extent.setSystemInfo("Organization", "TechMahindra-TR");
