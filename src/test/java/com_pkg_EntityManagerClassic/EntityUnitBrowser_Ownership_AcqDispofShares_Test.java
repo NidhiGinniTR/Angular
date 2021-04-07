7e8a06976e48f9595e52bb91b5e4bb13864dc4dd
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2021-04-07 15:45:19 +0530

    Added Acq-Disp of shares

---
+++ src/test/java/com_pkg_EntityManagerClassic/EntityUnitBrowser_Ownership_AcqDispofShares_Test.java
@@ -0,0 +1,207 @@
+package com_pkg_EntityManagerClassic;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchFrameException;
+import org.openqa.selenium.NoSuchWindowException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com_data_Resources.Environment.BrowserInvoke;
+import com_helper_Reporting.ExtentManager;
+import com_lib_FunctionLibrary.FrameWork;
+import com_lib_FunctionLibrary.loginPage;
+
+import com_obj_ObjectRepository.LS1.EntityUnitBrowser;
+import com_obj_ObjectRepository.LS1.LS1;
+import com_obj_ObjectRepository.OWM.OWM;
+
+public class EntityUnitBrowser_Ownership_AcqDispofShares_Test extends BrowserInvoke {
+	@BeforeSuite
+	public void beforeStart() {
+		ExtentManager.createInstance();
+	}
+
+	@Test
+	public void Initialize() throws IOException {
+		driver = InvokeDriver();
+		driver.get(propEnv.getProperty("URL"));
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+	}
+
+	@Test(dependsOnMethods = "Initialize")
+	public void Ownership_AcqDispofShares() throws InterruptedException, AWTException {
+		LS1 lp = new LS1(driver, propEnv, propSerialData);
+		FrameWork fm = new FrameWork();
+		EntityUnitBrowser Eub = new EntityUnitBrowser(driver, propEnv, propSerialData);
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(50))
+				.pollingEvery(Duration.ofSeconds(5))
+				.ignoring(NoSuchElementException.class,NoSuchWindowException.class)
+				.ignoring(NoSuchFrameException.class);
+		//---------LogIn-----------------------------
+			lp.fnLogin();
+		// Step-2:-----Launch Entity Unit Browser---------------------------//
+			lp.LaunchApplication("Entity Manager Classic");
+
+		// Step-3:-----Verify Search Fields---------------------------//
+		//driver.switchTo().defaultContent();
+			lp.fnSwitchtoWindow(2, "Entity Manager");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("gridFrame"));
+
+		// Step---3: Search and Navigate to Entity-------------//
+			Eub.fnSearchEntity();
+		
+			// Double click on required workflow
+			fm.fnWebTable(driver, driver.findElement(By.xpath("//tr[@id='gridEntityBrowser_grdEntityManager_row_0']")),
+				"DoubleClick");
+			Thread.sleep(2000);
+			lp.fnSwitchtoWindow(3, "Entity Information");
+			
+		// Step----------Navigateto Ownership tab to Acp-DispofShares--------------------------//
+			lp.fnEntitiesTabNavigation("Ownership", "Acq-Disp of Shares");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+			List<WebElement> rows = driver
+					.findElements(By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR"));
+			if (rows.size() >= 2) {
+				Robot robot = new Robot();
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				for (int i = 2; i <= rows.size(); i++) {
+					fm.fnWebTable(driver,
+							driver.findElement(
+									By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR[" + i + "]")),
+							"Click");
+				}
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+				lp.fnOWMActionsMenu("Delete", "");
+				Thread.sleep(1000);
+				fm.fnWebButton(driver, By.xpath("//*[@id='dialog-button-action']"), "OK");
+	
+			}
+
+		// Step----------Add New Acquisition-------------------------------//
+			fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+			lp.fnOWMActionsMenu("Add New Acquisition", "");
+			wait.until(ExpectedConditions.numberOfWindowsToBe(4));
+			lp.fnSwitchtoWindow(4, "Entity Manager");
+			Eub.fnEMAddNewAcquisitionDisposition();
+
+		// Step------------Add New Disposition-----------------------------//
+			wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+			lp.fnSwitchtoWindow(3, "Entity Information");
+			lp.fnEntitiesTabNavigation("", "Org-Reorg");
+			lp.fnEntitiesTabNavigation("", "Acq-Disp of Shares");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+			fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+			lp.fnOWMActionsMenu("Add New Disposition", "");
+			wait.until(ExpectedConditions.numberOfWindowsToBe(4));
+			lp.fnSwitchtoWindow(4, "Entity Manager");
+			Eub.fnEMAddNewAcquisitionDisposition();
+
+		// Step-----------Edit/View Details-------------------//
+			wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+			lp.fnSwitchtoWindow(3, "Entity Information");
+			lp.fnEntitiesTabNavigation("", "Org-Reorg");
+			lp.fnEntitiesTabNavigation("", "Acq-Disp of Shares");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+			List<WebElement> rowsafter = driver
+					.findElements(By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR"));
+			if (rowsafter.size() >= 2) {
+				for (int i = 2; i <= rowsafter.size(); i++) {
+					fm.fnWebTable(driver,
+							driver.findElement(
+									By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR[" + i + "]")),
+							"Click");
+					fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+					lp.fnOWMActionsMenu("Edit/View Details", "");
+					wait.until(ExpectedConditions.numberOfWindowsToBe(4));
+					lp.fnSwitchtoWindow(4, "Entity Manager");
+					Eub.fnEMEditAcquisitionDisposition();
+					wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+					lp.fnSwitchtoWindow(3, "Entity Information");
+					wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+				}
+			}
+
+		// Step----------Customize View--------------------------//
+			driver.switchTo().parentFrame();
+			lp.fnEntitiesTabNavigation("", "Org-Reorg");
+			lp.fnEntitiesTabNavigation("", "Acq-Disp of Shares");
+			Thread.sleep(1500);
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+			fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+			lp.fnOWMActionsMenu("Customize View", "");
+			Thread.sleep(3000);
+			lp.fnSwitchtoWindow(4, "Customize View");
+			String[] array = new String[] { "Type", "Data", "Number of Shares",
+					"Name of Shareholder Acquiring or Disposing"};
+			Eub.fnCustomizeView(array);
+
+		// Step----------Save Preferences------------------------//
+			lp.fnSwitchtoWindow(3, "Entity Information");
+			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("fraContent2"));
+			fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+			lp.fnOWMActionsMenu("Save Preferences", "");
+			Eub.fnSavePreferences("Save Preferences");
+
+		// Step----------Save Preferences for All----------------//
+			// lp.fnSwitchtoWindow(2, "Entity Information");
+			Thread.sleep(1000);
+			fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+			lp.fnOWMActionsMenu("Save Preferences for All", "");
+			Eub.fnSavePreferences("Save Preferences for All");
+
+		// Step-----------Delete---------------------------------//
+			List<WebElement> rows1 = driver
+					.findElements(By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR"));
+			System.out.println(rows1.size());
+			if (rows1.size() >= 2) {
+				Robot robot = new Robot();
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				for (int i = 2; i <= rows1.size(); i++) {
+					fm.fnWebTable(driver,
+							driver.findElement(
+									By.xpath("//DIV[@id='gridAds_grdEntityManager_dom']/TABLE[1]/TBODY[1]/TR[" + i + "]")),
+							"Click");
+				}
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				fm.fnWebButton(driver, By.xpath("//img[@id='btnActionsMenu']"), "Actions");
+				lp.fnOWMActionsMenu("Delete", "");
+				Thread.sleep(1000);
+				fm.fnWebButton(driver, By.xpath("//*[@id='dialog-button-action']"), "OK");
+	
+			}
+			driver.close();
+			wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+			lp.fnSwitchtoWindow(2, "Entity Manager");
+			lp.fnEntityManagerTabNavigation("Logout");
+
+		// Step--------LogOFF-------------------------------//
+			Eub.fnLogOff();
+	}
+
+	@AfterClass
+	void closeBrowser() throws InterruptedException {
+		driver.quit();
+	}
+
+	@AfterSuite
+	public void aftersuite() {
+		ExtentManager.endReport();
+	}
+}
