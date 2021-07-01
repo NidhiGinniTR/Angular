4f8e148e3139ed77b3847b7b6112127d0079c84d
x017401 <x017401@USE2DEVP0289.ead.thomsonreuters.com>
2021-07-01 20:19:28 +0530

    Updated Event Template and Holidays

--- src/main/java/com_obj_ObjectRepository/CalendarSetup/EventsTemplate.java
+++ src/main/java/com_obj_ObjectRepository/CalendarSetup/EventsTemplate.java
@@ -5,18 +5,24 @@ import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.aventstack.extentreports.Status;
+
 import com_helper_Reporting.ExtentManager;
 import com_lib_FunctionLibrary.FrameWork;
 
 public class EventsTemplate extends ExtentManager {
 
 	WebDriver driver;
-	FrameWork fm = new FrameWork();
+	
 	Properties data;
 	Properties template;
+	
+	public EventsTemplate(WebDriver driver, Properties data) {
+		this.driver = driver;
+		this.template = data;
+	}
 
-	// -------------------------------Event Template Search
-	// Criteria------------------
+	// -------------------------------Event Template SearchCriteria------------------
 	By CalSetup_EventTemplate_EventTemplateName = By.xpath("//*[@id='tce_event_name']");
 	By CalSetup_EventTemplate_EventDescription = By.xpath("//*[@id='tce_event_description']");
 	By CalSetup_EventTemplate_TaxType = By.xpath("//*[@id='tax_type_id']");
@@ -29,42 +35,78 @@ public class EventsTemplate extends ExtentManager {
 	By CalSetup_EventTemplate_FormType = By.xpath("//*[@id='form_type_id']");
 	By CalSetup_EventTemplate_Available = By.xpath("//*[@id='tce_visible']");
 	By CalSetup_EventTemplate_Obsolete = By.xpath("//*[@id='tce_obsolete']");
-	// ------------------------------Event Template Add
-	// New--------------------------
-	By CalSetup_Event_Template_ANET_EventTemplateName = By.xpath("//input[@id='txtRow1Col1']");
-	By CalSetup_Event_Template_ANET_Description = By.xpath("//input[@id='txtRow2Col1']");
-	By CalSetup_Event_Template_ANET_SearchFormAndAddress = By.xpath("//img[@id='imgLookupForms']");
-	By CalSetup_Event_Template_ANET_AddressName = By.xpath("//select[@id='ddlRow9Col1Txt']");
-	By CalSetup_Event_Template_ANET_RecurringEventChk = By.xpath("//input[@id='chkRow11Col1Row1Col1']");
-	By CalSetup_Event_Template_ANET_AdjustDueDate = By.xpath("//select[@id='ddlRow11aCol1']");
-	By CalSetup_Event_Template_ANET_Action = By.xpath("//select[@id='ddlRow12Col1']");
-	By CalSetup_Event_Template_ANET_PaymentMethod = By.xpath("//select[@id='ddlRow13Col1']");
-	By CalSetup_Event_Template_ANET_BasedOn = By.xpath("//select[@id='ddlRow1Col2']");
-	By CalSetup_Event_Template_ANET_Frequency = By.xpath("//select[@id='ddlRow2Col2']");
-	By CalSetup_Event_Template_ANET_UseMonths = By.xpath("//select[@id='ddlUseMonths']");
-	By CalSetup_Event_Template_ANET_UseDays = By.xpath("//select[@id='ddlUseDays']");
-	By CalSetup_Event_Template_ANET_Months = By.xpath("//input[@id='txtRow3Col2Mon']");
-	By CalSetup_Event_Template_ANET_Days = By.xpath("//input[@id='txtRow2Col2Day']");
-	By CalSetup_Event_Template_ANET_Add = By.xpath("//input[@id='cmdadd']");
-	By CalSetup_Event_Template_ANET_Replace = By.xpath("//input[@id='cmdReplace']");
-	By CalSetup_Event_Template_ANET_Remove = By.xpath("//input[@id='cmdRemove']");
-	By CalSetup_Event_Template_ANET_SelectedDates = By.xpath("//select[@id='lstRow4Col2dd']");
-	By CalSetup_Event_Template_ANET_AdjustDueDateToLastDayOfMonth = By.xpath("//input[@id='chkRow6Col2']");
-	By CalSetup_Event_Template_ANET_ForPrecedingTaxYear = By.xpath("input[@id='chkRow7Col2']");
-	By CalSetup_Event_Template_ANET_TaxYearForSchedulingIsBasedOn = By.xpath("//select[@id='ddlRow7bCol1']");
-	By CalSetup_Event_Template_ANET_FirstExtensionMonthsChk = By.xpath("//input[@id='txtRow4Col2ex']");
-	By CalSetup_Event_Template_ANET_FirstExtensionDaysChk = By.xpath("//input[@id='txtRow4Col3ex']");
-	By CalSetup_Event_Template_ANET_FirstExtensionADDTLDOMChk = By.xpath("//input[@id='chkext1Row4Col4']");
-	By CalSetup_Event_Template_ANET_SecondExtensionMonthsChk = By.xpath("//input[@id='txtRow5Col2ex']");
-	By CalSetup_Event_Template_ANET_SecondExtensionDaysChk = By.xpath("//input[@id='txtRow5Col3ex']");
-	By CalSetup_Event_Template_ANET_SecondExtensionADDTLDOMChk = By.xpath("//input[@id='chkext2Row5Col4']");
-	By CalSetup_Event_Template_ANET_ThirdExtensionMonthsChk = By.xpath("//input[@id='txtRow6Col2ex']");
-	By CalSetup_Event_Template_ANET_ThirdExtensionDaysChk = By.xpath("//input[@id='txtRow6Col3ex']");
-	By CalSetup_Event_Template_ANET_ThirdExtensionADDTLDOMChk = By.xpath("//input[@id='chkext3Row6Col4']");
-	By CalSetup_Event_Template_ANET_Notes = By.xpath("//textarea[@id='txtNotes']");
-	By CalSetup_Event_Template_ANET_AvailableChk = By.xpath("//input[@id='chkVisible']");
-	By CalSetup_Event_Template_ANET_ObsoleteChk = By.xpath("//input[@id='chkObsolete']");
-	By CalSetup_Event_Template_ANET_SaveBtn = By.xpath("//input[@id='cmdSave']");
-	By CalSetup_Event_Template_ANET_CancelBtn = By.xpath("//input[@id='cmdCancel']");
+	// ------------------------------Event Template AddNew--------------------------
+	By CalSetup_EventTemplate_AddNew_EventTemplateName = By.xpath("//input[@id='txtRow1Col1']");
+	By CalSetup_EventTemplate_AddNew_Description = By.xpath("//input[@id='txtRow2Col1']");
+	By CalSetup_EventTemplate_AddNew_SearchFormAndAddress = By.xpath("//img[@id='imgLookupForms']");
+	By CalSetup_EventTemplate_AddNew_AddressName = By.xpath("//select[@id='ddlRow9Col1Txt']");
+	By CalSetup_EventTemplate_AddNew_RecurringEventChk = By.xpath("//input[@id='chkRow11Col1Row1Col1']");
+	By CalSetup_EventTemplate_AddNew_AdjustDueDate = By.xpath("//select[@id='ddlRow11aCol1']");
+	By CalSetup_EventTemplate_AddNew_Action = By.xpath("//select[@id='ddlRow12Col1']");
+	By CalSetup_EventTemplate_AddNew_PaymentMethod = By.xpath("//select[@id='ddlRow13Col1']");
+	By CalSetup_EventTemplate_AddNew_BasedOn = By.xpath("//select[@id='ddlRow1Col2']");
+	By CalSetup_EventTemplate_AddNew_Frequency = By.xpath("//select[@id='ddlRow2Col2']");
+	By CalSetup_EventTemplate_AddNew_UseMonths = By.xpath("//select[@id='ddlUseMonths']");
+	By CalSetup_EventTemplate_AddNew_UseDays = By.xpath("//select[@id='ddlUseDays']");
+	By CalSetup_EventTemplate_AddNew_Months = By.xpath("//input[@id='txtRow3Col2Mon']");
+	By CalSetup_EventTemplate_AddNew_Days = By.xpath("//input[@id='txtRow2Col2Day']");
+	By CalSetup_EventTemplate_AddNew_Add = By.xpath("//input[@id='cmdadd']");
+	By CalSetup_EventTemplate_AddNew_Replace = By.xpath("//input[@id='cmdReplace']");
+	By CalSetup_EventTemplate_AddNew_Remove = By.xpath("//input[@id='cmdRemove']");
+	By CalSetup_EventTemplate_AddNew_SelectedDates = By.xpath("//select[@id='lstRow4Col2dd']");
+	By CalSetup_EventTemplate_AddNew_AdjustDueDateToLastDayOfMonth = By.xpath("//input[@id='chkRow6Col2']");
+	By CalSetup_EventTemplate_AddNew_ForPrecedingTaxYear = By.xpath("input[@id='chkRow7Col2']");
+	By CalSetup_EventTemplate_AddNew_TaxYearForSchedulingIsBasedOn = By.xpath("//select[@id='ddlRow7bCol1']");
+	By CalSetup_EventTemplate_AddNew_FirstExtensionMonthsChk = By.xpath("//input[@id='txtRow4Col2ex']");
+	By CalSetup_EventTemplate_AddNew_FirstExtensionDaysChk = By.xpath("//input[@id='txtRow4Col3ex']");
+	By CalSetup_EventTemplate_AddNew_FirstExtensionADDTLDOMChk = By.xpath("//input[@id='chkext1Row4Col4']");
+	By CalSetup_EventTemplate_AddNew_SecondExtensionMonthsChk = By.xpath("//input[@id='txtRow5Col2ex']");
+	By CalSetup_EventTemplate_AddNew_SecondExtensionDaysChk = By.xpath("//input[@id='txtRow5Col3ex']");
+	By CalSetup_EventTemplate_AddNew_SecondExtensionADDTLDOMChk = By.xpath("//input[@id='chkext2Row5Col4']");
+	By CalSetup_EventTemplate_AddNew_ThirdExtensionMonthsChk = By.xpath("//input[@id='txtRow6Col2ex']");
+	By CalSetup_EventTemplate_AddNew_ThirdExtensionDaysChk = By.xpath("//input[@id='txtRow6Col3ex']");
+	By CalSetup_EventTemplate_AddNew_ThirdExtensionADDTLDOMChk = By.xpath("//input[@id='chkext3Row6Col4']");
+	By CalSetup_EventTemplate_AddNew_Notes = By.xpath("//textarea[@id='txtNotes']");
+	By CalSetup_EventTemplate_AddNew_AvailableChk = By.xpath("//input[@id='chkVisible']");
+	By CalSetup_EventTemplate_AddNew_ObsoleteChk = By.xpath("//input[@id='chkObsolete']");
+	By CalSetup_EventTemplate_AddNew_Save = By.xpath("//input[@id='cmdSave']");
+	By CalSetup_EventTemplate_AddNew_Cancel = By.xpath("//input[@id='cmdCancel']");
+	
+	//-------------------------------------------------------------------------------------
+	FrameWork fm = new FrameWork();
+	
+	public void CalSetup_AddNewEventTemplate() {
+		childTest = test.createNode("Description: Add New Event Template " + "<br>"
+				+ "<<Screen Name: Calendar Setup -Event template -Add New Event Template >></br>");
+		try {
+				fm.fnWebButton(driver,CalSetup_EventTemplate_AddNew_Save ,"Save");
+				String popuptext = driver.switchTo().alert().getText();
+				if (popuptext.contains("Please fill in all required fields") || !popuptext.isEmpty()) {
+					driver.switchTo().alert().accept();
+					childTest.log(Status.PASS,
+							"Verification: Click on Save without entering details'" + popuptext + "' Alert/Message exists");
+				} else {
+					childTest.log(Status.ERROR,
+							"Verification: Click on Save without entering details, Alert/Message is missing");
+				}
+				fm.fnWebEdit(driver, CalSetup_EventTemplate_AddNew_EventTemplateName,data.getProperty("Eventtemplate_Name"), "Event Template Name");
+				fm.fnWebEdit(driver, CalSetup_EventTemplate_AddNew_Description,data.getProperty("Eventtemplate_Description"), "Description");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_AdjustDueDate,data.getProperty("Eventtemplate_AdjustDueDate"), "Adjust Due Date");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_Action,data.getProperty("Eventtemplate_Action"), "Action");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_PaymentMethod,data.getProperty("Eventtemplate_PaymentMethod"), "Payment Method");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_BasedOn,data.getProperty("Eventtemplate_BasedOn"), "Based On");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_Frequency,data.getProperty("Eventtemplate_Frequency"), "Frequency");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_UseMonths,data.getProperty("Eventtemplate_UseMonths"), "Use_Month");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_UseDays,data.getProperty("Eventtemplate_UseDays"), "Use_Days");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_Months,data.getProperty("Eventtemplate_Months"), "Months");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_Days,data.getProperty("Eventtemplate_Days"), "Days");
+				fm.fnWebButton(driver,CalSetup_EventTemplate_AddNew_Add ,"Add");
+				fm.fnWebList(driver, CalSetup_EventTemplate_AddNew_TaxYearForSchedulingIsBasedOn,data.getProperty("Eventtemplate_TaxYearforschedulingisbasedon"), "Tax Year for scheduling is based on");
+				fm.fnWebEdit(driver, CalSetup_EventTemplate_AddNew_Notes,data.getProperty("Eventtemplate_Notes"), "Notes");
+				fm.fnWebButton(driver,CalSetup_EventTemplate_AddNew_Save ,"Save");
+		}catch(Exception e) {
+			childTest.fail(e);
+		}
+	}
 
 }
--- src/main/java/com_obj_ObjectRepository/CalendarSetup/Holidays.java
+++ src/main/java/com_obj_ObjectRepository/CalendarSetup/Holidays.java
@@ -5,20 +5,26 @@ import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.aventstack.extentreports.Status;
+
 import com_helper_Reporting.ExtentManager;
 import com_lib_FunctionLibrary.FrameWork;
 
 public class Holidays extends ExtentManager {
 
 	WebDriver driver;
-	FrameWork fm = new FrameWork();
 	Properties data;
 	Properties template;
-	
-	//--------------------------------------Holidays Search Criteria
+
+	public Holidays(WebDriver driver, Properties data) {
+		this.driver = driver;
+		this.template = data;
+	}
+
+	// --------------------------------------Holidays Search Criteria
 	By CalSetup_Holidays_HolidayName = By.xpath("//*[@id='holiday_name']");
 	By CalSetup_Holidays_Country = By.xpath("//*[@id='country_id']");
-	//--------------------------------------Holidays Add New
+	// --------------------------------------Holidays Add New
 	By CalSetup_Holidays_AddNew_HolidayName = By.xpath("//*[@id='txtHolidayName']");
 	By CalSetup_Holidays_AddNew_Country = By.xpath("//*[@id='ddlCountry']");
 	By CalSetup_Holidays_AddNew_SelectedDate = By.xpath("//*[@id='txtSelectedDate']");
@@ -28,4 +34,57 @@ public class Holidays extends ExtentManager {
 	By CalSetup_Holidays_AddNew_Remove = By.xpath("//*[@id='cmdRemove']");
 	By CalSetup_Holidays_AdddNew_Save = By.xpath("//*[@id='btnSave']");
 	By CalSetup_Holidays_AddNew_Cancel = By.xpath("//*[@id='btnCancel']");
+
+	// ------------------------------------------------------------------
+
+	FrameWork fm = new FrameWork();
+	
+	
+
+	public void CalSetup_AddNewHoliday() {
+		childTest = test.createNode("Description: Add New Holiday Window " + "<br>"
+				+ "<<Screen Name: Calendar Setup - Holidays - Add New >></br>");
+		try {
+			fm.fnWebButton(driver, CalSetup_Holidays_AdddNew_Save, "Save");
+			String popuptext = driver.switchTo().alert().getText();
+			if (popuptext.contains("Please enter all required fields") || !popuptext.isEmpty()) {
+				driver.switchTo().alert().accept();
+				childTest.log(Status.PASS,
+						"Verification: Click on Save without entering details'" + popuptext + "' Alert/Message exists");
+			} else {
+				childTest.log(Status.ERROR,
+						"Verification: Click on Save without entering details, Alert/Message is missing");
+			}
+			fm.fnWebEdit(driver,CalSetup_Holidays_AddNew_HolidayName ,data.getProperty("HolidayName"),"Holiday Name");
+			fm.fnWebList(driver,CalSetup_Holidays_AddNew_Country ,data.getProperty("Holiday_Country"),"Country/Region");
+			fm.fnWebEdit(driver, CalSetup_Holidays_AddNew_SelectedDate,data.getProperty("Holiday_SelectedDate"),"Selected Date");
+			fm.fnWebButton(driver, CalSetup_Holidays_AddNew_Add, "Add");
+			Thread.sleep(500);
+			String text =getCalSetup_Holidays_AddNew_SelectedDates();
+			if(text.contains(data.getProperty("Holiday_SelectedDate"))) {
+				childTest.log(Status.INFO,"Selected date '"+data.getProperty("Holiday_SelectedDate")+"' has been added");
+			}else {
+				childTest.log(Status.ERROR,"Selected date '"+data.getProperty("Holiday_SelectedDate")+"' has not been added");
+			}
+			String popuptext1 = driver.switchTo().alert().getText();
+			if (popuptext1.contains("The Holiday has been successfully saved.") || popuptext.isEmpty()) {
+				driver.switchTo().alert().accept();
+				childTest.log(Status.PASS,
+						"Verification: Click on Save after entering details'" + popuptext1 + "' Alert/Message exists");
+			} else {
+				childTest.log(Status.ERROR,
+						"Verification: Click on Save after entering details, Alert/Message is missing");
+			}
+			
+		} catch (Exception e) {
+			childTest.fail(e);
+		}
+	}
+
+
+
+	private String getCalSetup_Holidays_AddNew_SelectedDates() {
+		return driver.findElement(CalSetup_Holidays_AddNew_SelectedDates).getText();
+	}
+
 }
